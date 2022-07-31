8d35305a2f80131a1301e133a911d26c5f802a15
proff116 <mak.nk16rus@gmail.com>
2022-07-31 12:50:11 +0300

    Add film list Update Films List UI

--- app/src/main/java/com/example/starwarswiki/ui/films/FilmsFragment.kt
+++ app/src/main/java/com/example/starwarswiki/ui/films/FilmsFragment.kt
@@ -6,14 +6,53 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.starwarswiki.databinding.FragmentFilmsBinding
+import com.example.starwarswiki.databinding.ItemFilmBinding
+import com.example.starwarswiki.model.Film
 import dagger.hilt.android.AndroidEntryPoint
+
+class FilmAdapter : RecyclerView.Adapter<FilmAdapter.ViewHolder>() {
+
+    private var items: List<Film> = emptyList()
+
+    fun addFilms(films: List<Film>) {
+        items = films
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(private val binding: ItemFilmBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(film: Film) {
+            binding.filmName.text = film.title
+            binding.filmEpisode.text = "Episode ${film.episodeId}"
+            binding.filmReleaseDate.text = film.releaseDate
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemFilmBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
 
 @AndroidEntryPoint
 class FilmsFragment : Fragment() {
 
     private val viewModel: FilmsViewModel by viewModels()
     private lateinit var binding: FragmentFilmsBinding
+    private val filmAdapter: FilmAdapter by lazy { FilmAdapter() }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,12 +60,19 @@ class FilmsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentFilmsBinding.inflate(inflater, container, false)
+        binding.filmList
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel.films.observe(viewLifecycleOwner) {
-            binding.textView.text = viewModel.films.value.toString()
+        binding.filmList.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = filmAdapter
+            setHasFixedSize(true)
+        }
+
+        viewModel.films.observe(viewLifecycleOwner) { films ->
+            filmAdapter.addFilms(films.results)
         }
     }
 }
